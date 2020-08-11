4853baaac4508d1a2facfb6cee976c01b35b6453
Francois de Klerk <francoisdk@bbd.co.za>
2020-08-11 09:48:58 +0200

    init

---
+++ src/main/java/com/face/frontendtester/FrontendtesterApplication.java
@@ -0,0 +1,13 @@
+package com.face.frontendtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendtesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendtesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/face/frontendtester/FrontendtesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.face.frontendtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendtesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
